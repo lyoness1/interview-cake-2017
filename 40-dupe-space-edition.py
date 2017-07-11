1d012ea481a1335cd0ac71a6ddd029bdce6335e7
Allison Lyon <allison.lyon@alation.com>
2017-07-11 14:31:19 -0700

    40 done

--- 40-dupe-space-edition.py
+++ 40-dupe-space-edition.py
@@ -1,8 +1,8 @@
 
 def find_dupe_space_optomized(arr):
-	"""
-	>>> find_dupe_space_optomized([1, 2, 3, 4, 5, 6, 6, 7, 8, 9])
-        [6]
+	"""Returns all duplicates in arr
+		>>> find_dupe_space_optomized([1, 2, 3, 4, 5, 6, 6, 7, 8, 9])
+		[6]
         >>> find_dupe_space_optomized([1, 2, 3, 3, 5, 6, 7, 7, 7, 9])
         [3, 7]
 	"""
@@ -12,6 +12,8 @@ def find_dupe_space_optomized(arr):
 		if arr[i] == arr[i+1] and arr[i] not in output:
 			output.append(arr[i])
 	return output
+
+
 
 
 if __name__ == '__main__':
